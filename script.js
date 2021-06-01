809a1bcb5d1ddd4707508392deb52f734a9487fe
luizrafa-federico <luizrafa.federico@gmail.com>
2021-06-01 14:31:28 -0500

    Fixing style and game logic

--- script.js
+++ script.js
@@ -1,42 +1,69 @@
-const section = document.getElementById("section");
-const tower1 = document.createElement("div");
-const tower2 = document.createElement("div");
-const tower3 = document.createElement("div");
-section.appendChild(tower1);
-section.appendChild(tower2);
-section.appendChild(tower3);
-tower1.classList.add("box", "box1", "wrapper");
-tower2.classList.add("box", "box2", "wrapper");
-tower3.classList.add("box", "box3", "wrapper");
-tower1.id = "box1";
-tower2.id = "box1";
-tower3.id = "box1";
+const content = document.querySelector('.content-wrapper');
+// TOWERS
+const tower1 = document.createElement('div');
+tower1.id = 'stack1';
+tower1.classList.add('wrapper', 'tower');
+content.appendChild(tower1)
 
-tower1.addEventListener("click", hide);
-tower2.addEventListener("click", hide);
-tower3.addEventListener("click", hide);
+const tower2 = document.createElement('div');
+tower2.id = 'stack2';
+tower2.classList.add('wrapper', 'tower');
+content.appendChild(tower2)
 
-function hide(e) {
-  console.log(e.currentTarget);
+const tower3 = document.createElement('div');
+tower3.id = 'stack3';
+tower3.classList.add('wrapper', 'tower');
+content.appendChild(tower3)
+
+const disc1 = document.createElement('div');
+disc1.id = 'box-1';
+disc1.classList.add('box', 'box1');
+tower1.appendChild(disc1);
+
+const disc2 = document.createElement('div');
+disc2.id = 'box-2';
+disc2.classList.add('box', 'box2');
+tower1.appendChild(disc2);
+
+const disc3 = document.createElement('div');
+disc3.id = 'box-3';
+disc3.classList.add('box', 'box3');
+tower1.appendChild(disc3);
+
+// DISCOS
+const disc4 = document.createElement('div');
+disc4.id = 'box-4';
+disc4.classList.add('box', 'box4');
+tower1.appendChild(disc4);
+
+const disc5 = document.createElement('div');
+disc5.id = 'box-5';
+disc5.classList.add('box', 'box5');
+tower1.appendChild(disc5);
+
+//GAME
+const towers = document.querySelectorAll('.tower');
+
+let isClick = false;
+let storedElm = undefined;
+
+const game = (e) => {
+    const tow = e.currentTarget; 
+    const lastElm = tow.lastElementChild;
+
+    if (isClick) {
+        isClick = false;
+        console.log('Clique 2')
+        tow.appendChild(storedElm);
+    } else {
+        isClick = true;
+        console.log('Clique 1')
+        storedElm = lastElm;
+    }
 }
+towers.forEach(tower => tower.addEventListener('click', game));
 
-const bar1 = document.createElement("div");
-const bar2 = document.createElement("div");
-const bar3 = document.createElement("div");
-const bar4 = document.createElement("div");
-bar1.id = "bar1";
-bar2.id = "bar2";
-bar3.id = "bar3";
-bar4.id = "bar4";
-bar1.classList.add("bar1", "shadow");
-bar2.classList.add("bar2", "shadow");
-bar3.classList.add("bar3", "shadow");
-bar4.classList.add("bar4", "shadow");
-tower1.appendChild(bar1);
-tower1.appendChild(bar2);
-tower1.appendChild(bar3);
-tower1.appendChild(bar4);
 
-section.addEventListener(click, listener);
 
-//crear la funcion jugar que reconozca 2 cllicks
+
+
