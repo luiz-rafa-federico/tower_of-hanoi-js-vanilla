1accff67cdb5d50e515f066b4a304dc3cd0dbee7
Glen Escobar <escobar.glen@gmail.com>
2021-06-01 22:38:07 -0300

    lastChild Width

--- script.js
+++ script.js
@@ -1,69 +1,69 @@
-const content = document.querySelector('.content-wrapper');
+const content = document.querySelector(".content-wrapper");
 // TOWERS
-const tower1 = document.createElement('div');
-tower1.id = 'stack1';
-tower1.classList.add('wrapper', 'tower');
-content.appendChild(tower1)
+const tower1 = document.createElement("div");
+tower1.id = "stack1";
+tower1.classList.add("wrapper", "tower");
+content.appendChild(tower1);
 
-const tower2 = document.createElement('div');
-tower2.id = 'stack2';
-tower2.classList.add('wrapper', 'tower');
-content.appendChild(tower2)
+const tower2 = document.createElement("div");
+tower2.id = "stack2";
+tower2.classList.add("wrapper", "tower");
+content.appendChild(tower2);
 
-const tower3 = document.createElement('div');
-tower3.id = 'stack3';
-tower3.classList.add('wrapper', 'tower');
-content.appendChild(tower3)
+const tower3 = document.createElement("div");
+tower3.id = "stack3";
+tower3.classList.add("wrapper", "tower");
+content.appendChild(tower3);
 
-const disc1 = document.createElement('div');
-disc1.id = 'box-1';
-disc1.classList.add('box', 'box1');
+const disc1 = document.createElement("div");
+disc1.id = "box-1";
+disc1.classList.add("box", "box1");
 tower1.appendChild(disc1);
 
-const disc2 = document.createElement('div');
-disc2.id = 'box-2';
-disc2.classList.add('box', 'box2');
+const disc2 = document.createElement("div");
+disc2.id = "box-2";
+disc2.classList.add("box", "box2");
 tower1.appendChild(disc2);
 
-const disc3 = document.createElement('div');
-disc3.id = 'box-3';
-disc3.classList.add('box', 'box3');
+const disc3 = document.createElement("div");
+disc3.id = "box-3";
+disc3.classList.add("box", "box3");
 tower1.appendChild(disc3);
 
 // DISCOS
-const disc4 = document.createElement('div');
-disc4.id = 'box-4';
-disc4.classList.add('box', 'box4');
+const disc4 = document.createElement("div");
+disc4.id = "box-4";
+disc4.classList.add("box", "box4");
 tower1.appendChild(disc4);
 
-const disc5 = document.createElement('div');
-disc5.id = 'box-5';
-disc5.classList.add('box', 'box5');
+const disc5 = document.createElement("div");
+disc5.id = "box-5";
+disc5.classList.add("box", "box5");
 tower1.appendChild(disc5);
 
 //GAME
-const towers = document.querySelectorAll('.tower');
+const towers = document.querySelectorAll(".tower");
 
 let isClick = false;
 let storedElm = undefined;
 
 const game = (e) => {
-    const tow = e.currentTarget; 
-    const lastElm = tow.lastElementChild;
+  const tow = e.currentTarget;
+  const lastElm = tow.lastElementChild;
+  //  const elem = e.lastElementChild;
 
-    if (isClick) {
-        isClick = false;
-        console.log('Clique 2')
-        tow.appendChild(storedElm);
-    } else {
-        isClick = true;
-        console.log('Clique 1')
-        storedElm = lastElm;
-    }
-}
-towers.forEach(tower => tower.addEventListener('click', game));
+  if (isClick) {
+    isClick = false;
+    console.log("Clique 2");
+    tow.appendChild(storedElm);
+    console.log(lastElm.clientWidth);
+  } else {
+    isClick = true;
+    console.log("Clique 1");
 
+    console.log(lastElm.clientWidth);
+    storedElm = lastElm;
+  }
+};
 
-
-
-
+towers.forEach((tower) => tower.addEventListener("click", game));
